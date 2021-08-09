60bf7de32446299804f179d4b33d0bbfa4a74398
Exo Des <hello@exodes.net>
2021-08-09 17:41:27 +0800

    feat: add express and middlewares

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,36 @@
 import * as functions from "firebase-functions";
+import admin from "firebase-admin";
+import express from "express";
+import cors from "cors";
+import cookieParse from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
 
-export const app = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+const app = express();
+
+// CORS
+const origins = [
+  "http://localhost:3000",
+  "http://localhost:4000",
+  "http://localhost:5000",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (origins.includes(origin!)) callback(null, true);
+    },
+  })
+);
+
+// Middleware
+app.use(helmet());
+app.use(cookieParse());
+app.use(morgan("dev"));
+
+// Routes
+app.get("/", (_, res) => {
+  res.send("OK");
 });
+
+export const api = functions.region("asia-southeast1").https.onRequest(app);
