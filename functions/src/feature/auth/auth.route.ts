e68a70d9ee156e5c5a92f8c4d17764b48f2e983f
Exo Des <hello@exodes.net>
2021-08-11 03:48:46 +0800

    feat: add auth verify route

--- functions/src/feature/auth/auth.route.ts
+++ functions/src/feature/auth/auth.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   authenticate,
   clearToken,
+  verifyAccess,
   create,
   createToken,
 } from "./auth.controller";
@@ -10,6 +11,7 @@ const authRoute = Router();
 
 authRoute.post("/create", create);
 authRoute.post("/authenticate", authenticate);
+authRoute.post("/verify", verifyAccess);
 authRoute.post("/token", createToken);
 authRoute.post("/clear", clearToken);
 
