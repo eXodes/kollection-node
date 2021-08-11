2047c2c2258890a9c74b0a09cdb3a470dfd1978c
Exo Des <hello@exodes.net>
2021-08-12 04:17:22 +0800

    feat: move POST `/auth/verify` route to GET `/auth` with authentication

--- functions/src/feature/auth/auth.route.ts
+++ functions/src/feature/auth/auth.route.ts
@@ -1,18 +1,19 @@
 import { Router } from "express";
+import { authentication } from "../../middleware/authentication";
 import {
+  verify,
   authenticate,
   clearToken,
-  verifyAccess,
   create,
-  createToken,
+  refreshToken,
 } from "./auth.controller";
 
 const authRoute = Router();
 
+authRoute.get("", authentication, verify);
 authRoute.post("/create", create);
 authRoute.post("/authenticate", authenticate);
-authRoute.post("/verify", verifyAccess);
-authRoute.post("/token", createToken);
+authRoute.post("/token", refreshToken);
 authRoute.post("/clear", clearToken);
 
 export { authRoute };
