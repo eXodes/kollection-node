a7cfdf35de6100bbb8e044d21822f3e3f8a0b81f
Exo Des <hello@exodes.net>
2021-08-12 04:30:14 +0800

    fix: enable helmet and cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import express from "express";
 import cors from "cors";
 import cookieParse from "cookie-parser";
 import morgan from "morgan";
-// import helmet from "helmet";
+import helmet from "helmet";
 import { authRoute } from "./feature/auth/auth.route";
 import { authentication } from "./middleware/authentication";
 
@@ -15,20 +15,20 @@ admin.initializeApp();
 export const db = admin.firestore();
 
 // CORS
-// const origins = [
-//   "http://localhost:3000",
-//   "http://localhost:4000",
-//   "http://localhost:5000",
-// ];
+const origins = [
+  "http://localhost:3000",
+  "http://localhost:4000",
+  "http://localhost:5000",
+];
 
 app.use(
   cors({
-    origin: true,
+    origin: origins,
   })
 );
 
 // Middleware
-// app.use(helmet());
+app.use(helmet());
 app.use(cookieParse());
 app.use(morgan("dev"));
 
